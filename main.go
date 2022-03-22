74eb0ce7f5ca1fd7ed13f57a1dcd5afcf86910d4
AgostinaM <agosmorellato@gmail.com>
2022-03-22 18:26:51 -0300

    Ej. 1 HTTP

---
+++ main.go
@@ -0,0 +1,27 @@
+package main 
+import (
+	"fmt"
+	"errors"
+)
+
+func main (){
+	
+	div, err := division(12, 6)
+
+	if err != nil {
+		
+		fmt.Println("Error:", err.Error())
+		
+		return 
+	}
+   fmt.Println("Division:", div)
+}
+
+func division (a , b int )(int, error){
+	if b == 0 {
+		return -1, errors.New("No se puede dividir por 0")
+	}
+     
+	 return  a / b, nil
+
+}

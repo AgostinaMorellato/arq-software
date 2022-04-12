42e042499b7f34ea1d2797ca351d72872ba4a7f2
AgostinaM <agosmorellato@gmail.com>
2022-04-12 16:20:59 -0300

    Ejercicios/Test division

--- division/div/div.go
+++ division/div/div.go
@@ -3,7 +3,7 @@ package div
 import "errors"
 
 
-func Division(a int, b int) (int, error) {
+func Division(a float64, b float64) (float64, error) {
 
 	if b == 0 {
           return 0, errors.New("no puedo dividir por 0")
--- division/div/div_test.go
+++ division/div/div_test.go
@@ -1,19 +1,74 @@
 package div
 
-import "testing"
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+type TestCase struct {
+	Name           string
+	A              float64
+	B              float64
+	ExpectedResult float64
+	ExpectedError  bool
+}
 
 func TestDivision(t *testing.T) {
-  
+	for _, testCase := range []TestCase{
+		{
+			Name:           "8/4 OK",
+			A:              8,
+			B:              4,
+			ExpectedResult: 2,
+			ExpectedError:  false,
+		},
+		{
+			Name:           "9/2 OK",
+			A:              9,
+			B:              2,
+			ExpectedResult: 4.5,
+			ExpectedError:  false,
+		},
+		{
+			Name:          "9/0 Error",
+			A:             9,
+			B:             0,
+			ExpectedError: true,
+		},
+	} {
+		t.Run(testCase.Name, func(t *testing.T) {
+			result, err := Division(testCase.A, testCase.B)
+			if testCase.ExpectedError {
+				assert.NotNil(t, err)
+				return
+			}
+			assert.EqualValues(t, testCase.ExpectedResult, result)
+		})
+	}
+}
+
+/*result, err := Division (8,4)
 	_, err := Division(8, 4)
 
 	if err != nil {
 		t.Error(err)
 		return
 	}
-
-}
-
-func TestDivisionErrorByZero(t *testing.T) {
+	assert.Nil(t, err)
+	// if result !=2 {
+	//t.Error(err)
+	//return }
+	assert.EqualValues(t, result, 2)
+	for 1
+    for i:= 0; i<= 10; i++{
+		fmt.Println(i)
+	}
+	//for 2 
+	values := []int{1,2,3,4,5,6}
+	for _, v := range values {
+		fmt.Println(v)
+	}*/
+/*func TestDivisionErrorByZero(t *testing.T) {
   
 	_, err := Division(8, 0)
 
@@ -22,4 +77,5 @@ func TestDivisionErrorByZero(t *testing.T) {
 		return
 	}
 
-}
+}*/
+
